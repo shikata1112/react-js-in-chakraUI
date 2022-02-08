b8128562b24b96e392aff6ad8c29de3f61d1783f
Shu Shikata <shu.42111227@gmail.com>
2022-02-08 23:42:18 +0900

    ログイン画面を作成

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,9 +1,29 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+  Stack
+} from "@chakra-ui/react";
 import { memo, VFC } from "react";
+
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: VFC = memo(() => {
   return (
-    <div>
-      <p>ログインページです</p>
-    </div>
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={8} py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <PrimaryButton>ログイン</PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
   );
 });

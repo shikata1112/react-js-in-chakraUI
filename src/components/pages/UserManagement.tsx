b2a1a7b8d74762bcbc34c61b61ffbabccf2c8301
Shu Shikata <shu.42111227@gmail.com>
2022-02-09 20:20:26 +0900

    ユーザー一覧の表示を実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,9 +1,33 @@
-import { memo, VFC } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react";
+
+import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), [getUsers]);
+
   return (
-    <div>
-      <p>ユーザー管理ページです</p>
-    </div>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="space-around">
+          {users.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                ImageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
